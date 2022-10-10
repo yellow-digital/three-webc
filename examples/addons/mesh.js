d529e8eebc78cd6c5595f93a22dcf8b8a722c2a1
Jelle <jellehak@gmail.com>
2022-10-10 16:31:16 +0200

    better mesh

--- examples/addons/mesh.js
+++ examples/addons/mesh.js
@@ -1,29 +1,41 @@
 import * as THREE from "three";
 
 customElements.define(
-    "t-mesh",
-    class extends HTMLElement {
-      async connectedCallback() {
-        setTimeout(this.mounted.bind(this));
+  "t-mesh",
+  class extends HTMLElement {
+    async connectedCallback() {
+      setTimeout(this.mounted.bind(this));
+    }
+    mounted() {
+      const view = this.parentElement.viewport;
+      const { scene } = view;
+
+      const type = `${this.getAttribute("geo")}Geometry`;
+      const geometry = new THREE[type]();
+      const material = new THREE.MeshStandardMaterial({
+        color: 0x00ff00,
+        side: 2,
+      });
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.name = "object";
+      scene.add(mesh);
+
+      // Apply attributes
+      {
+        const fn = new Function(`return ${this.getAttribute(":rotation")}`);
+        const rotation = {...fn()};
+        mesh.rotation.x = rotation.x || 0;
+        mesh.rotation.y = rotation.y || 0;
+        mesh.rotation.z = rotation.z || 0;
       }
-      mounted() {
-        const view = this.parentElement.viewport;
-        const { scene } = view;
-  
-        const type = `${this.getAttribute("geo")}Geometry`;
-        const geometry = new THREE[type]();
-        const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, side: 2 });
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.name = "object";
-        scene.add(mesh);
-  
-        // Apply attributes
-        const fn = new Function(`return ${this.getAttribute(":rotation")}`)
-        const rotation = fn()
-        mesh.rotation.x = rotation.x || 0
-        mesh.rotation.y = rotation.y || 0
-        mesh.rotation.z = rotation.z || 0
+
+      {
+        const fn = new Function(`return ${this.getAttribute(":position")}`);
+        const position = {...fn()};
+        mesh.position.x = position.x || 0;
+        mesh.position.y = position.y || 0;
+        mesh.position.z = position.z || 0;
       }
     }
-  );
-  
+  }
+);
