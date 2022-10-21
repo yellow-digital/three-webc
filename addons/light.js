acc527522a7dcb5b9556e3b9c0adf985274a023e
Jelle <jellehak@gmail.com>
2022-10-21 16:12:52 +0200

    t-light

---
+++ addons/light.js
@@ -0,0 +1,16 @@
+import * as THREE from "three";
+import { ThreeWebc } from "three-webc";
+
+class Element extends ThreeWebc.Element {
+  constructor() {
+    super();
+    const light = new THREE[this.getAttribute('type')]();
+    this.light = light
+  }
+
+  async mounted() {
+    this.scene.add( this.light );
+  }
+}
+
+ThreeWebc.define("light", Element);
