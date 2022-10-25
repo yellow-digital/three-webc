0ba8e547926a657f7587eee8116a4a966ac75ddb
Jelle <jellehak@gmail.com>
2022-10-25 16:52:26 +0200

    general purpose object component

--- addons/index.js
+++ addons/index.js
@@ -1,8 +1,12 @@
+/*
+NOTE load order is important. This defines the construction order of the webcomponents. For example material should be above mesh.
+*/
 export * from "./camera.js"
 export * from "./effects.js"
 export * from "./geo.js"
 export * from "./geometry.js"
 export * from "./material.js"
+export * from "./style.js"
 export * from "./mesh.js"
 export * from "./model.js"
 export * from "./object3d.js"
@@ -10,3 +14,4 @@ export * from "./studio.js"
 export * from "./overlay.js"
 export * from "./stats.js"
 export * from "./light.js"
+export * from "./object.js"
