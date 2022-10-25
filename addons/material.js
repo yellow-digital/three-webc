fbb705a8387119752ac2f5a531cbb75cb9c81b7c
Jelle <jellehak@gmail.com>
2022-10-25 08:41:22 +0200

    setup

--- addons/material.js
+++ addons/material.js
@@ -2,6 +2,11 @@ import * as THREE from "three";
 import { ThreeWebc } from "three-webc";
 
 export class Material extends ThreeWebc.Element {
+  constructor() {
+    super()
+    this.material = {}
+  }
+
   mounted() {
     const type = `${this.getAttribute("type")}Material`;
     if (!THREE[type]) {
@@ -9,13 +14,14 @@ export class Material extends ThreeWebc.Element {
     }
 
     const material = new THREE[type]({
-      color: this.getAttribute("color") || "",
+      color: this.color || this.getAttribute("color") || "",
       side: 2,
+      // Mixin initial settings
+      ...this.material
     });
     this.material = material;
 
     // Attach
-    // Resolve parent
     const parent = this.parentElement;
     parent.material = material;
   }
